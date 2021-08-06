919b60779087d58511d84d7adbb09327ffa83913
Swithun Crowe <cs2@st-andrews.ac.uk>
2021-08-06 15:38:11 +0100

    start work on database dump web interface

---
+++ public/SIFIDS_ADMIN/Dump.php
@@ -0,0 +1,68 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SIFIDS_ADMIN;
+
+class Dump {
+    private $action = NULL;
+    private $startDate = NULL;
+    private $endDate = NULL;
+    private $db = NULL;
+    
+    public function __construct(string $action) { //{{{
+        $this->action = $action;
+        
+        $this->db = new DB();
+        $this->db->setFetch(\PDO::FETCH_NUM);
+    }
+    
+    public function setStartDate(string $d) { //{{{
+        $this->startDate = $d;
+    }
+    //}}}
+
+    public function setEndDate(string $d) { //{{{
+        $this->endDate = $d;
+    }
+    //}}}
+    
+    public function __toString() : string { //{{{
+        $filename = '';
+        
+        // get results from database
+        $results = $this->{$this->action}($filename);
+        
+        // open memory file handle
+        $fh = fopen('php://memory', 'r+');
+        
+        // write CSV data to file handle
+        foreach ($results as $row) {
+            fputcsv($fh, $row);
+        }
+        
+        // finished
+        rewind($fh);
+        
+        // headers for CSV as attachment
+        header('Content-type: text/csv');
+        header(sprintf('Content-disposition: attachment; filename=%s.csv',
+                       $filename));
+        
+        // send back CSV as string
+        return stream_get_contents($fh);
+    }
+    //}}}
+
+    // return data on trips made within date range
+    private function trips() : array { //{{{
+        if (!$this->startDate || !$this->endDate) {
+            throw new \Exception('Need start and end dates for trips data');
+        }
+        
+        return $this->db->dumpTrips($this->startDate, $this->endDate);
+    }
+    //}}}
+}
+
+?>
---
+++ public/admin/dump.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace SIFIDS_ADMIN;
+
+require_once '../autoload.php';
+
+$dumpActions = ['trips', 'tracks', 'vessels'];
+$dumpForm = 'dump.html';
+
+if (isset($_GET['dump']) && in_array($_GET['dump'], $dumpActions)) {
+    $dump = new Dump($_GET['dump']);
+    
+    // optional start/end dates
+    if (isset($_GET['start_date'])) {
+        $dump->setStartDate($_GET['start_date']);
+    }
+    
+    if (isset($_GET['end_date'])) {
+        $dump->setEndDate($_GET['end_date']);
+    }
+    
+    try {
+        print $dump;
+    }
+    catch (\Exception $e) {
+        print $e->getMessage();
+    }
+
+    exit;
+}
+else {
+    readfile($dumpForm);
+}
+
+?>
