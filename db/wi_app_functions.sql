407ff50879ce763131ea188a8dcefda865db7375
Swithun Crowe <cs2@st-andrews.ac.uk>
2021-08-26 16:57:14 +0100

    changes to creels table

--- db/wi_app_functions.sql
+++ db/wi_app_functions.sql
@@ -130,7 +130,7 @@ INNER JOIN entities."Animals" AS a
             END
       FROM JSON_TO_RECORDSET(in_json -> 'creels') AS j
            ("date" TIMESTAMP, latitude NUMERIC(15, 12), longitude NUMERIC(15, 12),
-            notes TEXT)
+            notes TEXT, "incidentType" VARCHAR(16), "gearType" VARCHAR(8))
  LEFT JOIN entities."IncidentTypes" AS i ON (i.incidentTypeName = j."incidentType")
  LEFT JOIN entities."IncidentGearType" AS ig ON (ig.incidentGearTypeName = j."gearType");
             
