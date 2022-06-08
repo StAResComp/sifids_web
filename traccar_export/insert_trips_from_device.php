13ae917ad937557d39b68afcad1e96be87edf29f
Swithun Crowe <cs2@st-andrews.ac.uk>
2022-06-08 14:33:52 +0100

    script to export trip data from Traccar

---
+++ traccar_export/insert_trips_from_device.php
@@ -0,0 +1,80 @@
+#!/usr/bin/php
+<?php
+
+declare(strict_types=1);
+
+namespace TRACCAR2;
+
+require_once '../public/autoload.php';
+
+// read CSV data piped from STDIN
+
+// get device ID using IMEI
+function getID(string $imei) : int { //{{{
+		global $db;
+		
+		if (!$results = $db->getDeviceID($imei)) {
+				throw new \Exception('Unknown device');
+		}
+		
+		return (int) $results[0]->device_id;
+}
+//}}}
+
+// turn row into JSON object
+function makeJSON(array $row) : \stdClass { //{{{
+    global $attributes, $ids;
+    
+    $json = new \stdClass();
+    $json->position = new \stdClass();
+    
+    $json->position->deviceTime = $row['devicetime'];
+    $json->position->uniqueId = $row['uniqueid'];
+    $json->position->attributes = json_decode($row['attributes']);
+    $json->position->latitude = (float) $row['latitude'];
+    $json->position->longitude = (float) $row['longitude'];
+    $json->position->valid = 't' == $row['valid'] ? 1 : 0;
+}
+//}}}
+
+// connect to DB with transaction
+$db = DB::getInstance(true);
+
+// array for remembering device IDs from IMEI numbers
+$ids = [];
+
+// get available attributes
+if (!$results = $db->getAttributes()) {
+    throw new \Exception('Problem getting attributes');
+}
+
+foreach ($results as $row) {
+    $attributes[] = $row->attribute_name;
+}
+
+// open STDIN
+$fh = fopen('php://stdin', 'r');
+
+// loop over lines from STDIN
+// IMEI, timestamp, speed, course
+while (false !== ($row = fgetcsv($fh))) {
+		try {
+				// remember device IDs from IMEIs
+				if (!isset($ids[$row[0]])) {
+						$ids[$row[0]] = getID($row[0]);
+				}
+				
+        $json = makeJSON($row);
+        
+        print json_encode($json);
+        break;
+		}
+		catch (\Throwable $e) {
+				printf("%s\n", $e->getMessage());
+				continue;
+		}
+}
+
+fclose($fh);
+
+?>
