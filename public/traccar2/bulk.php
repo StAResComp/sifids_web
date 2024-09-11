b6a300519054530201c38b0fc970f990cda5d035
Swithun Crowe <cs2@st-andrews.ac.uk>
2024-09-11 09:36:09 +0100

    get positions with new format event 385 attributes

--- public/traccar2/bulk.php
+++ public/traccar2/bulk.php
@@ -8,10 +8,24 @@ require_once '../autoload.php';
 require_once 'functions.php';
 
 // attributes of device to record in database
-$attributes = array('power', 'distance', 'totalDistance');
+//$attributes = array('power', 'distance', 'totalDistance');
+$attributes = [];
+
+// allowed major/minor numbers
+//$allowedMajorMinor = ['020b010a'];
+$allowedMajorMinor = ['020b010a', 'ed650055', 'd72e0055', 'f3630055'];
 
 try {
     $db = DB::getInstance(true);
+
+    // get available attributes
+    if (!$results = $db->getAttributes()) {
+        throw new \Exception('Problem getting attributes');
+    }
+    
+    foreach ($results as $row) {
+        $attributes[] = $row->attribute_name;
+    }
 
     while (($stdin = fgets(STDIN)) != false) {
         if ($data = json_decode($stdin)) {
