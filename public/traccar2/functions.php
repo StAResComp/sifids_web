d274c1e16645a0070e17cdb5928f776b6dbda471
Swithun Crowe <cs2@st-andrews.ac.uk>
2024-09-11 09:18:37 +0100

    forward data from some trackers to new API

--- public/traccar2/functions.php
+++ public/traccar2/functions.php
@@ -4,6 +4,155 @@ declare(strict_types=1);
 
 namespace TRACCAR2;
 
+// post JSON to new API
+function newAPI(string $json) { //{{{
+		// API endpoint to post JSON to
+		$url = 'https://localhost/api/traccar_position/';
+		
+		// post JSON but don't check host/cert
+		$opts = [CURLOPT_HTTPHEADER =>
+						 ['Content-Type: application/json'],
+						 CURLOPT_POST => true,
+						 CURLOPT_RETURNTRANSFER => true,
+						 CURLOPT_SSL_VERIFYPEER => false,
+						 CURLOPT_SSL_VERIFYHOST => false,
+						 CURLOPT_POSTFIELDS => $json];
+		
+		$curl = curl_init($url);
+		curl_setopt_array($curl, $opts);
+		$ret = curl_exec($curl);
+		
+		// throw exception if 201 isn't response code
+		if (201 != (int) curl_getinfo($curl, CURLINFO_RESPONSE_CODE)) {
+				throw new \Exception(curl_error($curl));
+		}
+}
+//}}}
+
+// connect to given database using ~/.pgpass
+function dbConnect(string $host, string $db, string $user, int $port=5432) : \PDO { //{{{
+    $pw = '';
+    $pgpass = file('/home/sifids/.pgpass', FILE_IGNORE_NEW_LINES);
+    foreach ($pgpass as $line) {
+        $cells = explode(':', $line);
+        if ($cells[0] != $host || $cells[1] != $port ||
+            $cells[2] != $db || $cells[3] != $user) {
+            continue;
+        }
+        
+        $pw = $cells[4];
+        break;
+    }
+    
+    return new \PDO(sprintf('pgsql:host=%s;port=%d;dbname=%s',
+                            $host, $port, $db), $user, $pw);
+}
+//}}}
+
+function slice(string $str, int $p) : array { //{{{
+    return [substr($str, 0, $p), substr($str, $p)];
+}
+//}}}
+
+// parse io385 data and add to database
+function io385(string $data, int $deviceID, string $timestamp) { //{{{
+    global $db, $allowedMajorMinor;
+    
+    // strip of first 2 characters
+    list($ignore, $data) = slice($data, 2);
+    
+    while ($data) {
+        // read BLE
+        list($ble, $data) = slice($data, 2);
+        $ble = (int) base_convert($ble, 16, 10);
+        
+        $signalPresent = (bool) $ble & 1;
+        $format = $ble & 32 == 32 ? 'iBeacon' : 'Eddystone';
+        
+        switch ($format) {
+         case 'iBeacon':
+            $signal = NULL;
+            list($uuid, $data) = slice($data, 32);
+            list($major, $data) = slice($data, 4);
+            list($minor, $data) = slice($data, 4);
+            
+            if ($signalPresent) {
+                list($signal, $data) = slice($data, 2);
+                $signal = (int) base_convert($signal, 16, 10) - 256; // signed 2 complement
+            }
+            
+            // check that major/minor are recognised
+            if (in_array($major . $minor, $allowedMajorMinor)) {
+                $db->addCoinData($deviceID,
+                                 $timestamp,
+                                 $uuid, $major, $minor,
+                                 $signal);
+            }
+            
+            break;
+            
+         case 'Eddystone':
+            throw new \Exception('Cannot read Eddystone beacon data');
+            break;
+        }
+    }
+}
+//}}}
+
+// handle new format of io385 event
+function event385(\stdClass $attrs, int $deviceID, string $timestamp) { //{{{
+    global $db, $allowedMajorMinor;
+    
+    $fmts = ['uuid' => 'beacon%dUuid', 'major' => 'beacon%dMajor',
+             'minor' => 'beacon%dMinor', 'signal' => 'beacon%dRssi'];
+
+    // start beacons at 0
+    $b = 0;
+    
+    while (true) {
+        // move to next beacon
+        ++ $b;
+        
+        $values = [];
+        
+        // check that all values for beacon are present
+        foreach ($fmts as $f => $v) {
+            $val = sprintf($v, $b);
+            if (!isset($attrs-> {$val})) {
+                break 2; // missing so finished
+            }
+            $values[$f] = $attrs->{$val};
+            
+            switch ($f) {
+             case 'major':
+             case 'minor':
+                $values[$f] = base_convert((string) $values[$f], 10, 16);
+                if (1 == strlen($values[$f]) % 2) {
+                    $values[$f] = sprintf('0%s', $values[$f]);
+                }
+                break;
+             default:
+                break;
+            }
+        }
+        
+        // major/minor values not in white list
+        if (!in_array($values['major'] . $values['minor'], 
+                      $allowedMajorMinor)) {
+            printf("%s%s not in %s\n", $values['major'], $values['minor'], 
+                   implode(',', $allowedMajorMinor));
+            continue;
+        }
+        
+        // add to database
+        $db->addCoinData($deviceID,
+                         $timestamp,
+                         $values['uuid'], $values['major'], $values['minor'],
+                         $values['signal']);
+    }
+}
+//}}}
+
 // add data from JSON object to database
 function addData(\stdClass $data) { //{{{
     global $attributes, $db;
@@ -15,22 +164,27 @@ function addData(\stdClass $data) { //{{{
     if (!isset($data->position->deviceTime)) {
         throw new \Exception('Need time from device');
     }
-    
-    // need track point
-    if (!isset($data->position->latitude) ||
-        !isset($data->position->longitude)) {
-        throw new \Exception('Need latitude and longitude');
-    }
-    
-    $timestamp = $data->position->deviceTime;
-    
+
     // get device ID
     if (!$results = $db->getDeviceID($data->device->uniqueId)) {
-        throw new \Exception('Couldn\'t get device ID');
+        throw new \Exception(sprintf('Couldn\'t get device ID %s', 
+                                     $data->device->uniqueId));
     }
     
     $deviceID = $results[0]->device_id;
     
+    $timestamp = $data->position->deviceTime;
+    
+    // check for event io385 - bluetooth coin data
+    if (isset($data->position->attributes->io385) &&
+        strlen($data->position->attributes->io385) > 2) {
+        io385($data->position->attributes->io385, $deviceID, $timestamp);
+    }
+    if (isset($data->position->attributes->event) &&
+        385 == $data->position->attributes->event) {
+        event385($data->position->attributes, $deviceID, $timestamp);
+    }
+
     // add attributes
     foreach ($attributes as $name) {
         if (isset($data->position->attributes->$name)) {
@@ -38,6 +192,13 @@ function addData(\stdClass $data) { //{{{
                               (float) $data->position->attributes->$name,
                               $timestamp, $deviceID);
         }
+    }
+    
+    // if no track point then finished
+    if (!isset($data->position->latitude) ||
+        !isset($data->position->longitude)) {
+        //throw new \Exception('Need latitude and longitude');
+        return;
     }
     
     // get trip ID, possibly creating new trip
--- public/traccar2/index.php
+++ public/traccar2/index.php
@@ -7,6 +7,20 @@ namespace TRACCAR2;
 require_once '../autoload.php';
 require_once 'functions.php';
 
+// allowed major/minor numbers
+//$allowedMajorMinor = ['020b010a'];
+$allowedMajorMinor = ['020b010a', 'ed650055', 'd72e0055', 'f3630055'];
+
+// IMEIs to forward to new API
+$imeis = ['354017112212382',
+					'358480085730432',
+					'354017112212317',
+					'359632104292831',
+					'359632104296600',
+					'354017112212234',
+					'354017112196833',
+					'866907053435003'];
+
 // attributes of device to record in database
 $attributes = array(); //'power', 'distance', 'totalDistance', 'sat', 'battery');
 
@@ -26,6 +40,10 @@ try {
     $fh = fopen('php://input', 'r');
     $stdin = stream_get_contents($fh);
     fclose($fh);
+
+    file_put_contents('/tmp/traccar.json', 
+                      print_r($_POST, true), 
+                      FILE_APPEND);
     
     if (!$stdin) {
         throw new \Exception('No input given');
@@ -34,14 +52,20 @@ try {
     if (!$data = json_decode($stdin)) {
         throw new \Exception('Data not in JSON format');
     }
+		
+		// pass JSON to new API?
+		if (in_array($data->device->uniqueId, $imeis)) {
+				newAPI($stdin);
+		}
+		else {
     
-    /*file_put_contents('/tmp/traccar.json', 
+/*    file_put_contents('/home/sifids/dump.json', 
                       json_encode($data, JSON_PRETTY_PRINT), 
                       FILE_APPEND);*/
-    //error_log(json_encode($data, JSON_PRETTY_PRINT));
     
-    // convert input JSON string to object and add to database
-    addData($data);
+				// convert input JSON string to object and add to database
+				addData($data);
+		}
 }
 catch (\Throwable $e) {
     error_log($e->getMessage() . "\n");
